5c1ff9c679cbc2fb966decf5d6b83aeda79f1d1d
Daniel <gleek.dan@gmail.com>
2021-03-02 20:08:27 -0700

    Add x-forwarded-for

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
-  res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+  // res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+  res.json({ipaddress: req.headers['x-forwarded-for'], language: req.headers["accept-language"], software: req.headers["user-agent"]});
 });
 
 
